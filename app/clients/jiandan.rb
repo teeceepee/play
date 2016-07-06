1f10209c0166084bc647eefb847f42665d6f022d
Yu Haidong <yuhaidongyhd@gmail.com>
2016-07-06 23:53:07 +0800

    Update Jiandan client

--- app/clients/jiandan.rb
+++ app/clients/jiandan.rb
@@ -1,23 +1,43 @@
 class Jiandan
   URL_BASE = 'http://jandan.net/ooxx'
   # URL_BASE = 'http://jandan.net/ooxx/page-1'
-
-  def self.test
-    self.new.save_newest
-  end
+  LIMIT = 5
 
   def initialize
     @newest_page_doc = self.class.fetch(URL_BASE)
+    @docs = {
+      max_page => @newest_page_doc
+    }
   end
 
   # @return [Integer]
   def max_page
-    @newest_page_doc.at_css('.comments .current-comment-page').text.gsub(/\D/, '').to_i
+    self.class.doc_page(@newest_page_doc)
   end
 
-  def save_newest
-    newest_comments.each do |comment_data|
-      save_comment(comment_data)
+  def stored_max_page
+    c = JiandanComment.where("json ? 'page'").order("json->'page' desc").first
+    c.present? ? c.page : nil
+  end
+
+  # 增量保存
+  def save_comments_incr
+    page_range = if stored_max_page.present?
+      (stored_max_page..max_page)
+    else
+      ((max_page - LIMIT)..max_page)
+    end
+
+    save_comments(page_range)
+  end
+
+  # @param pages [Range|nil]
+  def save_comments(pages = nil)
+    range = pages || (max_page..max_page)
+    range.each do |page|
+      page_comments(page).each do |comment_data|
+        save_comment(comment_data)
+      end
     end
     nil
   end
@@ -32,11 +52,15 @@ class Jiandan
   end
 
   # @return [Array]
-  def newest_comments
-    self.class.parse_comments(@newest_page_doc)
+  def page_comments(page)
+    if @docs[page].blank?
+      @docs[page] = self.class.fetch(self.class.page_url(page))
+    end
+    self.class.parse_comments(@docs[page])
   end
 
   def self.parse_comments(doc)
+    page = doc_page(doc)
     doc.css('ol.commentlist li[id^=comment]').map do |el|
       comment_id = el.attr('id').split('-').last
       author = el.at_css('.row .author strong').text
@@ -58,6 +82,7 @@ class Jiandan
         code: code,
         time: time,
         urls: urls,
+        page: page,
       }
     end
   end
@@ -66,5 +91,17 @@ class Jiandan
     resp = HTTP.get(url)
     body = resp.body.to_s
     Nokogiri::HTML(body)
+  end
+
+  def self.page_url(page = nil)
+    if page.to_i > 1
+      "#{URL_BASE}/page-#{page}"
+    else
+      URL_BASE
+    end
+  end
+
+  def self.doc_page(doc)
+    doc.at_css('.comments .current-comment-page').text.gsub(/\D/, '').to_i
   end
 end
--- app/models/jiandan_comment.rb
+++ app/models/jiandan_comment.rb
@@ -1,2 +1,4 @@
 class JiandanComment < ApplicationRecord
+
+  store_accessor :json, [:comment_id, :page]
 end
