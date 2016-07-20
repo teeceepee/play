d1383ada0cb09da04c4376e540877f645dce941a
Yu Haidong <yuhaidongyhd@gmail.com>
2016-07-20 22:27:35 +0800

    Select larger image urls

--- app/models/jiandan_comment.rb
+++ app/models/jiandan_comment.rb
@@ -5,7 +5,26 @@ class JiandanComment < ApplicationRecord
   # @return [Array]
   def self.all_urls
     JiandanComment.all.order(created_at: :desc).map do |comment|
-      comment.urls['thumbs']
+      comment.better_urls
     end.flatten
+  end
+
+  def better_urls
+    h = [self.urls['thumbs'], self.urls['originals']].flatten.compact.group_by do |url|
+      url.split('/').last
+    end
+
+    h.values.map do |url_array|
+      if url_array.size == 1
+        url_array.first
+      else
+        selected = url_array.select { |url| url.match(/large|mw600/)}
+        if selected.size >= 2
+          selected.find { |url| url.match(/large/) }
+        else
+          url_array.first
+        end
+      end
+    end
   end
 end
