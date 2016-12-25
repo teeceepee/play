8524d54841b3ae34222bb5951a2f1ccd2f96e05a
Yu Haidong <yuhaidongyhd@gmail.com>
2016-12-25 23:16:24 +0800

    Modify i18n-js Capistrano task

--- config/deploy.rb
+++ config/deploy.rb
@@ -80,13 +80,20 @@ task :echo_path do
   end
 end
 
-# Load i18n-js rake task
+# i18n-js
 namespace :i18n do
   namespace :js do
     desc 'Export translations to JS file(s)'
     task :export do
-      I18n::JS.export
+      on release_roles(fetch(:assets_roles)) do
+        within release_path do
+          with rails_env: fetch(:rails_env) do
+            execute :rake, 'i18n:js:export'
+          end
+        end
+      end
     end
   end
+
+  before 'deploy:compile_assets', 'i18n:js:export'
 end
-before 'deploy:compile_assets', 'i18n:js:export'
