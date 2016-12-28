283b6deeab6c669f60546256416fa160d7b1bbed
Yu Haidong <yuhaidongyhd@gmail.com>
2016-12-28 11:39:11 +0800

    Add chat notification

--- app/assets/javascripts/pages/chat.js
+++ app/assets/javascripts/pages/chat.js
@@ -1,8 +1,31 @@
 //= require cable
 
+Notification.requestPermission(function (permission) {
+  console.log("permission: " + permission)
+})
+
 $(document).ready(function () {
+  displayNo = function (message) {
+    n = new Notification("New message", {
+      icon: 'http://i0.hdslb.com/bfs/archive/07d57f5ead6702d0f5ef9d5b7e5d778b9ce67062.jpg',
+      body: message,
+      tag: "tag",
+    })
+
+    n.onclick = function (e) {
+      console.log(e)
+    }
+
+    n.onerror = function (e) {
+      console.log(e)
+    }
+  }
+
   chatChannel.received = function (data) {
-    $('<div class="alert alert-info"></div>').text(data.message).hide().appendTo($(".messages")).fadeIn()
+    var message = data.message
+    $('<div class="alert alert-info"></div>').text(message).hide().appendTo($(".messages")).fadeIn()
+
+    displayNo(message)
   }
 
   $("#form").on("submit", function (e) {
