bcef42aa513328bf85b498a9fc317c066d538113
Yu Haidong <yuhaidongyhd@gmail.com>
2017-01-02 21:46:38 +0800

    Set the correct WebSocket urls

--- app/assets/javascripts/pages/web_socket.js
+++ app/assets/javascripts/pages/web_socket.js
@@ -1,6 +1,20 @@
 
-var echoSocket = new WebSocket("ws://localhost:3000/ws/echo_server")
-var broadcastSocket = new WebSocket("ws://localhost:3000/ws/broadcast_server")
+// copied from ActionCable
+var createWebSocketURL = function(url) {
+  var a
+  if (url && !/^wss?:/i.test(url)) {
+    a = document.createElement("a")
+    a.href = url
+    a.href = a.href
+    a.protocol = a.protocol.replace("http", "ws")
+    return a.href
+  } else {
+    return url
+  }
+}
+
+var echoSocket = new WebSocket(createWebSocketURL("/ws/echo_server"))
+var broadcastSocket = new WebSocket(createWebSocketURL("/ws/broadcast_server"))
 
 var sockets = {
   echo: echoSocket,
