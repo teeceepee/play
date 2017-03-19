7b6adc05e8d9d45e93f8158b80007725d6d0adfa
Yu Haidong <yuhaidongyhd@gmail.com>
2017-03-19 18:27:24 +0800

    Add Elasticsearch mapping

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,21 @@
 class Article < ApplicationRecord
   include Elasticsearch::Model if defined?(Elasticsearch)
 
+  # elasticsearch-plugin install analysis-smartcn
+  mapping do
+    # indexes :id, type: 'long'
+
+    indexes :title, type: 'text' do
+      indexes :chinese, type: 'text', analyzer: 'smartcn'
+      indexes :keyword, type: 'text'
+    end
+    indexes :content, type: 'text' do
+      indexes :chinese, type: 'text', analyzer: 'smartcn'
+      indexes :keyword, type: 'text'
+    end
+
+    indexes :created_at, type: 'date'
+    indexes :updated_at, type: 'date'
+  end
 
 end
