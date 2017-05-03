447ad93d327d4e8ec7d5b402935bb799dfe05ed5
Yu Haidong <yuhaidongyhd@gmail.com>
2017-05-04 00:48:05 +0800

    Add React animation

--- app/javascript/packs/index_page.js
+++ app/javascript/packs/index_page.js
@@ -2,11 +2,20 @@ require("../pages/index_page/style.scss")
 
 import React from "react"
 import ReactDOM from "react-dom"
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
 
 import { name } from "../constants"
 
 const Index = props => (
-  <h1>Welcome to {props.name}</h1>
+  <CSSTransitionGroup
+    transitionName="index-title"
+    transitionAppear={true}
+    transitionAppearTimeout={1000}
+    transitionEnter={false}
+    transitionLeave={false}
+  >
+    <h1 >Welcome to {props.name}</h1>
+  </CSSTransitionGroup>
 )
 
 Index.defaultProps = {
