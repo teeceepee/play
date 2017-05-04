fd7bf65b59b73c856dfda5f69f721c0fa3ed164b
Yu Haidong <yuhaidongyhd@gmail.com>
2017-05-04 23:39:54 +0800

    Migrate bilibili gifs page to webpack

---
+++ app/javascript/packs/bilibili_gifs.js
@@ -0,0 +1,139 @@
+require("../pages/bilibili_gifs/style.scss")
+
+import React from "react"
+import ReactDOM from "react-dom"
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
+
+var Gif = React.createClass({
+  displayName: 'Gif',
+
+  getInitialState: function () {
+    return {
+      selected: false
+    };
+  },
+
+  handleClick: function () {
+    this.setState({
+      selected: true
+    });
+
+    setTimeout(function () {
+      this.setState({
+        selected: false
+      })
+    }.bind(this), 1500);
+  },
+
+  handleMouseEnter: function () {
+    this.setState({
+      selected: true
+    });
+  },
+
+  handleMouseLeave: function () {
+    this.setState({
+      selected: false
+    });
+  },
+
+  href: function() {
+    return this.props.gif.links[0];
+  },
+
+  isNew: function() {
+    var now = Math.floor(Date.now() / 1000);
+    var postTime = Number.parseInt(this.props.gif.posttime);
+    return now - postTime < (3 * 24 * 3600);
+  },
+
+  isSelected: function () {
+    return this.state.selected;
+  },
+
+  img: function() {
+    var img = React.createElement('img', {
+      src: this.props.gif.icon,
+      title: this.props.gif.title,
+      alt: this.props.gif.title,
+      onClick: this.handleClick,
+      onMouseEnter: this.handleMouseEnter,
+      onMouseLeave: this.handleMouseLeave,
+    });
+
+    return React.createElement(CSSTransitionGroup, {
+      transitionName: "gif-transition",
+      transitionAppear: true,
+      transitionAppearTimeout: 5000,
+    }, React.createElement('div', null, img));
+
+  },
+
+  title: function() {
+    return React.createElement(
+      'div',
+      null,
+      React.createElement(
+        'span',
+        null,
+        this.props.gif.id + ' '
+      ),
+      React.createElement(
+        'a',
+        {
+          className: 'search-link',
+          href: this.href(),
+          target: '_blank'
+        },
+        this.props.gif.title
+      )
+    );
+  },
+
+  a: function() {
+    return React.createElement('a', {title: this.props.gif.title, target: '_blank'}, this.img());
+  },
+
+  render: function() {
+    var classes = ['gif'];
+    if (this.isNew()) {
+      classes.push('new');
+    }
+    if (this.isSelected()) {
+      classes.push('selected')
+    }
+
+    return React.createElement(
+      'div',
+      {className: classes.join(' ')},
+      this.title(),
+      this.a());
+  }
+});
+
+var GifList = React.createClass({
+  displayName: 'GifList',
+
+  gifs: function(gifs) {
+    return gifs.map(function(gif) {
+      return React.createElement(Gif, {gif: gif})
+    });
+  },
+  render: function() {
+    var nodes = this.gifs(this.props.gifs);
+    return React.createElement('div', {className: 'gif-list'}, nodes);
+  }
+});
+
+var renderAll = function(gifs) {
+  ReactDOM.render(
+    React.createElement(GifList, {gifs: gifs}),
+    document.getElementById('gifs-container')
+  );
+};
+
+fetch('/d/bilibili_gifs')
+  .then((response) => response.json())
+  .then((json) => {
+    renderAll(json.fix)
+  })
