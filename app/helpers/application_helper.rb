31bfcdc7ace5747fbd70af5ba9003a226c11255f
Yu Haidong <yuhaidongyhd@gmail.com>
2017-06-10 13:59:01 +0800

    Update GA script

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@ module ApplicationHelper
   def rails_root_comment
     "<!-- #{Rails.root.basename} -->"
   end
+
+  def current_domain
+    @_current_domain ||= request.domain(1)
+  end
+
+  def ga_tracking_id
+    if current_domain == 'xiumaijia.com'
+      'UA-100812996-1'
+    else
+      'UA-67854007-2'
+    end
+  end
 end
