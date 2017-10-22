a35d2919e46932e4710951a1e49a05ecb27c2f2d
Yu Haidong <yuhaidongyhd@gmail.com>
2017-10-22 19:23:59 +0800

    Disable Jiandan fetcher

--- app/clients/fetcher.rb
+++ app/clients/fetcher.rb
@@ -2,7 +2,7 @@ class Fetcher
   IOS_USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_1_1 like Mac OS X) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0 Mobile/14B100 Safari/602.1'
 
   def self.fetch_all
-    Jiandan.fetch_new
+    # Jiandan.fetch_new
     Hupu.fetch_recent_news
     # Weibo.fetch_cookie
     # self.bitcoin
