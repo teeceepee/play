ff50f940fa9e923f80e6ccea660eb9dfed763587
Yu Haidong <yuhaidongyhd@gmail.com>
2018-01-07 12:15:06 +0800

    Improve API

--- app/controllers/d/nba_games_controller.rb
+++ app/controllers/d/nba_games_controller.rb
@@ -3,11 +3,18 @@ class D::NbaGamesController < ApplicationController
   def index
     year = params[:year]
     month = params[:month]
+    day = params[:day]
     utc_offset = params[:utc_offset] || '+08:00'
 
     if year.present? && month.present?
-      from = Time.new(year, month, nil, nil, nil, nil, utc_offset)
-      to = from + 1.month
+      if day.present?
+        from = Time.new(year, month, day, nil, nil, nil, utc_offset)
+        to = from + 1.day
+      else
+        from = Time.new(year, month, nil, nil, nil, nil, utc_offset)
+        to = from + 1.month
+      end
+
       nba_games = NbaGame.between_range(from, to)
 
       render_json(nba_games)
