b85bd0bdb3390d58b07944432f2b68ac06d7d5e6
Yu Haidong <yuhaidongyhd@gmail.com>
2019-04-30 22:00:11 +0800

    Add TSLint

--- app/javascript/consts.ts
+++ app/javascript/consts.ts
@@ -1,7 +1,7 @@
 
-const foo = "foo"
-const bar = () => "bar"
-const name = (): string => "play"
+const foo = 'foo'
+const bar = () => 'bar'
+const name = (): string => 'play'
 
 export {
   foo,
--- app/javascript/utils/http.ts
+++ app/javascript/utils/http.ts
@@ -1,11 +1,11 @@
-import axios from "axios"
+import axios from 'axios'
 
 const token: string = document.querySelector('[name=csrf-token]')!.getAttribute('content') || ''
 
 export default axios.create({
   headers: {
     // Accept: "application/json",
-    "X-Requested-With": "XMLHttpRequest",
-    "X-CSRF-Token": token,
-  }
+    'X-CSRF-Token': token,
+    'X-Requested-With': 'XMLHttpRequest',
+  },
 })
