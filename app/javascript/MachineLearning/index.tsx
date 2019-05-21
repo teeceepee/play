26cd4eae51e87a79862747d37d4630044090e2d9
Yu Haidong <yuhaidongyhd@gmail.com>
2019-05-21 21:45:27 +0800

    Import ml5 dynamically

--- app/javascript/MachineLearning/index.tsx
+++ app/javascript/MachineLearning/index.tsx
@@ -1,14 +1,40 @@
 import React, { PureComponent } from 'react'
-import { ImageClassifier } from './ImageClassifier'
+import * as $script from 'scriptjs'
 
-class MachineLearning extends PureComponent {
+interface State {
+  component: React.ComponentType | null
+}
 
-  render() {
+class MachineLearning extends PureComponent<{}, State> {
+
+  public state: State = {
+    component: null,
+  }
+
+  public componentDidMount () {
+    $script('https://unpkg.com/ml5@0.1.1/dist/ml5.min.js', async () => {
+      const module = await import('./ImageClassifier')
+
+      this.setState({
+        component: module.ImageClassifier,
+      })
+    })
+  }
+
+  public render () {
     return (
       <div className="container mt-4">
-        <ImageClassifier/>
+        {this.inner()}
       </div>
     )
+  }
+
+  private inner () {
+    if (this.state.component != null) {
+      return React.createElement(this.state.component)
+    } else {
+      return <div>Loading...</div>
+    }
   }
 }
 
